069a3db1ec336f350cb0027657f0d5a024208c9a
ezen-test <ezen-test@ezen-test-PC>
2019-06-12 15:22:54 +0900

    confilc

--- spring_boot_winni/src/main/java/com/whine/winni/repository/Artwork_repository.java
+++ spring_boot_winni/src/main/java/com/whine/winni/repository/Artwork_repository.java
@@ -4,17 +4,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.whine.winni.vo.Admin_vo;
+import com.whine.winni.vo.Artwork_vo;
+
 
 @Repository
 public class Artwork_repository {
 
 	@Autowired
 	private ArtworkRepository_interface repository_interface;
-	
-	
-	//check admin account
+
+	// check admin account
+
 	public int check_admin(Admin_vo account) {
 		return repository_interface.check_admin(account.getId(), account.getPwd());
 	}
 	
+	public void insert_artwork(Artwork_vo artwork) {
+		repository_interface.save(artwork);
+	}
+	
+
 }
